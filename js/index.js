677962b25ff9a3dec0c805965f50480ea9870f36
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-10-27 00:13:20 +0700

    Render Articles New

--- js/index.js
+++ js/index.js
@@ -4,12 +4,13 @@ const API = axios.create({
 
 const elMainMenu = document.getElementById("mainMenu");
 const elArticlesTrending = document.getElementById("articlesTrending");
+const elArticlesNew = document.getElementById("articlesNew");
+const elArticleNewLarge = document.getElementById("articleNewLarge");
 
 // RENDER MENUS
 API.get(`categories_news`).then((response) => {
   const data = response.data;
   const categories = data.data;
-  //   console.log(categories);
 
   let htmlMenu = "";
   let htmlMenuOther = "";
@@ -37,7 +38,6 @@ API.get(`categories_news`).then((response) => {
 //RENDER ARTICLES TRENDING
 API.get(`articles/popular?limit=5`).then((response) => {
   const articles = response.data.data;
-  console.log(articles);
 
   let html = "";
   articles.forEach((item, index) => {
@@ -54,4 +54,76 @@ API.get(`articles/popular?limit=5`).then((response) => {
   });
 
   elArticlesTrending.innerHTML = html;
+});
+
+//RENDER ALL ARTICLE NEW
+API.get(`articles?limit=5`).then((response) => {
+  const articles = response.data.data;
+  console.log(articles);
+
+  let html = "";
+  articles.forEach((item, index) => {
+    if (index === 0) {
+      elArticleNewLarge.innerHTML = /*html*/ `
+        <div class="post-entry-1 lg">
+        <a href="#"
+          ><img
+            src="${item.thumb}"
+            alt="${item.title}"
+            class="img-fluid"
+        /></a>
+        <div class="post-meta">
+          <span class="date">${item.category.name}</span>
+          <span class="mx-1">&bullet;</span> 
+          <span>${item.publish_date}</span>
+        </div>
+        <h2>
+          <a href="#"
+            >${item.title}</a
+          >
+        </h2>
+        <p class="mb-4 d-block">
+          ${item.description}
+        </p>
+
+        <div class="d-flex align-items-center author">
+          <div class="photo">
+            <img
+              src="assets/img/person-1.jpg"
+              alt=""
+              class="img-fluid"
+            />
+          </div>
+          <div class="name">
+            <h3 class="m-0 p-0">${item.author}</h3>
+          </div>
+        </div>
+      </div>
+        `;
+    } else {
+      html += /*html*/ ` 
+    <div class="col-lg-6">
+      <div class="post-entry-1">
+        <a href="#"
+          ><img
+            src="${item.thumb}"
+            alt="${item.title}"
+            class="img-fluid"
+        /></a>
+        <div class="post-meta">
+          <span class="date">${item.category.name}</span>
+          <span class="mx-1">&bullet;</span>
+          <span>${item.publish_date}</span>
+        </div>
+        <h2>
+          <a href="#"
+            >${item.title}</a
+          >
+        </h2>
+      </div>
+    </div>`;
+    }
+  });
+
+  elArticlesNew.innerHTML = html;
 });
