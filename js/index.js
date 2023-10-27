ea7ef44db82fc53df4b630e77896cfddff63aacc
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-10-28 01:04:10 +0700

    Show Category Featured

--- js/index.js
+++ js/index.js
@@ -73,179 +73,19 @@ API.get("categories_news/articles?limit_cate=2&limit=9").then((response) => {
 
   let html = "";
   data.forEach((item, index) => {
-    html += /*html*/ `<section class="category-section">
-<div class="container" data-aos="fade-up">
-  <div
-    class="section-header d-flex justify-content-between align-items-center mb-5"
-  >
-    <h2>${item.name}</h2>
-    <div>
-      <a href="category.html" class="more">See All Lifestyle</a>
-    </div>
-  </div>
+    const categoryName = item.name;
+    const articles = item.articles;
 
-  <div class="row g-5">
-    <div class="col-lg-8">
-      <div class="row g-5">
-        <div class="col-lg-6">
-          <div class="post-entry-1">
-            <a href="single-post.html"
-              ><img
-                src="assets/img/post-landscape-6.jpg"
-                alt=""
-                class="img-fluid"
-            /></a>
-            <div class="post-meta">
-              <span class="date">Lifestyle</span>
-              <span class="mx-1">&bullet;</span>
-              <span>Jul 5th '22</span>
-            </div>
-            <h2>
-              <a href="single-post.html"
-                >Let’s Get Back to Work, New York</a
-              >
-            </h2>
-          </div>
+    html += /*html*/ `
+    <section class="category-section">
+        <div class="container" data-aos="fade-up">
+          ${renderCategorySectionTitle(categoryName)}
+          ${renderArticlesByCategoryFeatured(articles)}
         </div>
-        <div class="col-lg-6">
-          <div class="post-entry-1">
-            <a href="single-post.html"
-              ><img
-                src="assets/img/post-landscape-5.jpg"
-                alt=""
-                class="img-fluid"
-            /></a>
-            <div class="post-meta">
-              <span class="date">Lifestyle</span>
-              <span class="mx-1">&bullet;</span>
-              <span>Jul 17th '22</span>
-            </div>
-            <h2>
-              <a href="single-post.html"
-                >How to Avoid Distraction and Stay Focused During
-                Video Calls?</a
-              >
-            </h2>
-          </div>
-        </div>
-        <div class="col-lg-6">
-          <div class="post-entry-1">
-            <a href="single-post.html"
-              ><img
-                src="assets/img/post-landscape-3.jpg"
-                alt=""
-                class="img-fluid"
-            /></a>
-            <div class="post-meta">
-              <span class="date">Lifestyle</span>
-              <span class="mx-1">&bullet;</span>
-              <span>Jul 5th '22</span>
-            </div>
-            <h2>
-              <a href="single-post.html"
-                >6 Easy Steps To Create Your Own Cute Merch For
-                Instagram</a
-              >
-            </h2>
-          </div>
-        </div>
-        <div class="col-lg-6">
-          <div class="post-entry-1">
-            <a href="single-post.html"
-              ><img
-                src="assets/img/post-landscape-2.jpg"
-                alt=""
-                class="img-fluid"
-            /></a>
-            <div class="post-meta">
-              <span class="date">Lifestyle</span>
-              <span class="mx-1">&bullet;</span>
-              <span>Mar 1st '22</span>
-            </div>
-            <h2>
-              <a href="single-post.html"
-                >10 Life-Changing Hacks Every Working Mom Should
-                Know</a
-              >
-            </h2>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-4 border-start custom-border">
-      <div class="post-entry-1 border-bottom">
-        <div class="post-meta">
-          <span class="date">Lifestyle</span>
-          <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span>
-        </div>
-        <h2 class="mb-2">
-          <a href="single-post.html"
-            >How to Avoid Distraction and Stay Focused During Video
-            Calls?</a
-          >
-        </h2>
-        <span class="author mb-3 d-block">Jenny Wilson</span>
-      </div>
-
-      <div class="post-entry-1 border-bottom">
-        <div class="post-meta">
-          <span class="date">Lifestyle</span>
-          <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span>
-        </div>
-        <h2 class="mb-2">
-          <a href="single-post.html"
-            >17 Pictures of Medium Length Hair in Layers That Will
-            Inspire Your New Haircut</a
-          >
-        </h2>
-        <span class="author mb-3 d-block">Jenny Wilson</span>
-      </div>
-
-      <div class="post-entry-1 border-bottom">
-        <div class="post-meta">
-          <span class="date">Lifestyle</span>
-          <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span>
-        </div>
-        <h2 class="mb-2">
-          <a href="single-post.html"
-            >9 Half-up/half-down Hairstyles for Long and Medium
-            Hair</a
-          >
-        </h2>
-        <span class="author mb-3 d-block">Jenny Wilson</span>
-      </div>
-
-      <div class="post-entry-1 border-bottom">
-        <div class="post-meta">
-          <span class="date">Lifestyle</span>
-          <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span>
-        </div>
-        <h2 class="mb-2">
-          <a href="single-post.html"
-            >Life Insurance And Pregnancy: A Working Mom’s Guide</a
-          >
-        </h2>
-        <span class="author mb-3 d-block">Jenny Wilson</span>
-      </div>
-      <div class="post-entry-1 border-bottom">
-        <div class="post-meta">
-          <span class="date">Lifestyle</span>
-          <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span>
-        </div>
-        <h2 class="mb-2">
-          <a href="single-post.html"
-            >Life Insurance And Pregnancy: A Working Mom’s Guide</a
-          >
-        </h2>
-        <span class="author mb-3 d-block">Jenny Wilson</span>
-      </div>
-    </div>
-  </div>
-  <!-- End .row -->
-</div>
-</section>
+    </section>
 `;
   });
+
   elCategoriesFeturedWithArticles.innerHTML = html;
 });
 
@@ -323,4 +163,76 @@ function renderArticleNewItem(item) {
         </h2>
       </div>
     </div>`;
+}
+
+function renderCategorySectionTitle(categoryName) {
+  return /*html*/ `
+<div class="section-header d-flex justify-content-between align-items-center mb-5">
+   <h2>${categoryName}</h2>
+   <div>
+     <a href="category.html" class="more">See All ${categoryName}</a>
+   </div>
+</div>
+  `;
+}
+
+function renderArticlesByCategoryFeatured(articles) {
+  let htmlArticlesLeft = "";
+  let htmlArticlesRight = "";
+
+  articles.forEach((articleItem, index) => {
+    const title = articleItem.title;
+    const thumb = articleItem.thumb;
+    const publishDate = articleItem.publish_date;
+    const author = articleItem.author;
+
+    if (index < 4) {
+      htmlArticlesLeft += /*html*/ `
+      <div class="col-lg-6">
+        <div class="post-entry-1">
+          <a href="#"
+            ><img
+              src="${thumb}"
+              alt="${title}"
+              class="img-fluid"
+          /></a>
+          <div class="post-meta">
+            <span>${publishDate}</span>
+          </div>
+          <h2>
+            <a href="#"
+              >Let’s Get Back to Work, New York</a
+            >
+          </h2>
+        </div>
+      </div>
+    `;
+    } else {
+      htmlArticlesRight += /*html*/ `
+    <div class="post-entry-1 border-bottom">
+      <div class="post-meta">
+      <span>${publishDate}</span>
+      </div>
+      <h2 class="mb-2">
+        <a href="#"
+          >${title}</a
+        >
+      </h2>
+      <span class="author mb-3 d-block">${author}</span>
+    </div>
+      `;
+    }
+  });
+
+  return /*html*/ `
+  <div class="row g-5">
+    <div class="col-lg-8">
+      <div class="row g-5">
+      ${htmlArticlesLeft}
+      </div>
+    </div>
+    <div class="col-lg-4 border-start custom-border">
+      ${htmlArticlesRight}
+    </div>
+  </div>`;
 }
