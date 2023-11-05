4e63ce2e69f54df187beea1ab52685df659c8452
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-11-05 23:47:49 +0700

    Save the 4 most recently viewed articles to LS

--- js/detail.js
+++ js/detail.js
@@ -8,13 +8,31 @@ const elPublishDate = document.getElementById("publishDate");
 const elArticleTitle = document.getElementById("articleTitle");
 const elArticleContent = document.getElementById("articleContent");
 
-API.get(`articles/${id}`).then((response) => {
-  console.log(response.data);
-  const article = response.data.data;
+let RECENT_POST = JSON.parse(localStorage.getItem("RECENT_POST")) || [];
 
-  elCategoryName.innerText = article.category.name;
-  elPublishDate.innerText = article.publish_date;
-  elArticleTitle.innerText = article.title;
-  elArticleContent.innerHTML = article.content;
-  elArticleThumb.src = article.thumb;
-});
+console.log(RECENT_POST);
+API.get(`articles/${id}`)
+  .then((response) => {
+    console.log(response.data);
+    const article = response.data.data;
+
+    elCategoryName.innerText = article.category.name;
+    elPublishDate.innerText = article.publish_date;
+    elArticleTitle.innerText = article.title;
+    elArticleContent.innerHTML = article.content;
+    elArticleThumb.src = article.thumb;
+
+    //Save the 4 most recently viewed articles to LocalStorage
+    if (!RECENT_POST.includes(id)) {
+      if (RECENT_POST.length === 4) {
+        //Delete bai dau
+        RECENT_POST.shift();
+      }
+      RECENT_POST.push(id);
+      //Save Id in LocalStorage
+      localStorage.setItem("RECENT_POST", JSON.stringify(RECENT_POST));
+    }
+  })
+  .catch((error) => {
+    window.location.href = "index.html";
+  });
