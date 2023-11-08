334af4a8644bf3dfdcad0196e757b5eb70a27ab2
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-11-08 23:38:28 +0700

    register success

--- js/register.js
+++ js/register.js
@@ -2,21 +2,27 @@ const elAuthForm = document.getElementById("authForm");
 const elFormMessage = document.getElementById("formMessage");
 const elEmail = document.getElementById("email");
 const elPassword = document.getElementById("password");
+const elName = document.getElementById("name");
+const elPhone = document.getElementById("phone");
+const elAddress = document.getElementById("address");
 
 elAuthForm.addEventListener("submit", function (e) {
   e.preventDefault();
 
   const email = elEmail.value.trim();
   const password = elPassword.value.trim();
+  const name = elName.value.trim();
+  const phone = elPhone.value.trim();
+  const address = elAddress.value.trim();
 
-  const data = { email, password };
+  const data = { name, email, password, phone, address };
   console.log(data);
 
-  API.post("auth/login", data)
+  API.post("users/register", data)
     .then((response) => {
       window.location.href = "index.html";
     })
     .catch((error) => {
-      elFormMessage.innerHTML = `<div class="alert alert-danger" role="alert">Thông tin đăng nhập không đúng, vui lòng thử lại!</div>`;
+      elFormMessage.innerHTML = `<div class="alert alert-danger" role="alert">Đăng ký thất bại, vui lòng nhập đúng thông tin!</div>`;
     });
 });
