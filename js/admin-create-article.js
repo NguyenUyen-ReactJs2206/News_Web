4305891aed7f755c60dc55df1d975380eb61ff4f
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-11-14 14:57:04 +0700

    Using Unsplash API to get image random

--- js/admin-create-article.js
+++ js/admin-create-article.js
@@ -1,3 +1,7 @@
+//Access Key    j7A0MN2L2as_93aEMLHaQM7EUWPgAaAf4xjRN-UWb9g
+//Secret key    WIT6KtYL_UM_7eiBQaCNE3dy-G56VdauyAU96qCDVPE
+
+//https://api.unsplash.com/photos/random?client_id=j7A0MN2L2as_93aEMLHaQM7EUWPgAaAf4xjRN-UWb9g&orientation=landscape
 API.callWithToken()
   .get("auth/me")
   .then((response) => {})
@@ -9,6 +13,20 @@ const elAuthForm = document.getElementById("authForm");
 const elFormMessage = document.getElementById("formMessage");
 const elThumb = document.getElementById("thumb");
 const elThumbPreview = document.getElementById("thumPreview");
+const elBtnRandomThumb = document.getElementById("btnRandomThumb");
+
+elBtnRandomThumb.addEventListener("click", function () {
+  API.call()
+    .get(
+      "https://api.unsplash.com/photos/random?client_id=j7A0MN2L2as_93aEMLHaQM7EUWPgAaAf4xjRN-UWb9g&orientation=landscape"
+    )
+    .then((response) => {
+      console.log(response);
+      const urlThumb = response.data.urls.regular;
+      elThumb.value = urlThumb;
+      elThumbPreview.src = urlThumb;
+    });
+});
 
 elThumb.addEventListener("change", function () {
   if (elThumb.value) elThumbPreview.src = elThumb.value;
